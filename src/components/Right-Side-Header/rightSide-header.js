a31795910deb2c7fd3a5eff605dc755846bf67d0
Luiz <lalmeida89@gmail.com>
2020-06-16 18:13:20 -0400

    searchbar and dropdown transitions

--- src/components/Right-Side-Header/rightSide-header.js
+++ src/components/Right-Side-Header/rightSide-header.js
@@ -9,13 +9,20 @@ export const RightSideHeader = props => {
 
   return (
     <div className='rightSide-header'>
-      <div className='rostersBtn'
-        onClick={()=>showRosters()}>
-        <span> Team <br/> Rosters </span>
+      <div className='rosters-button-outerDiv rightSide-button-outerDiv'>
+        <button className='rosters-button rightSide-button'
+          onClick={()=>showRosters()}>
+          <i className="fas fa-clipboard-list rightSide-button-icon"></i>
+
+          <span className='rightSide-button-label'> Team <br/> Rosters </span>
+        </button>
       </div>
-      <div className='draftedPlayersBtn'
-        onClick={()=>showDraftedPlayers()}>
-        <span> Drafted <br/> Players </span>
+      <div className='draftedPlayers-button-outerDiv rightSide-button-outerDiv'>
+        <button className='draftedPlayers-button rightSide-button'
+          onClick={()=>showDraftedPlayers()}>
+          <i className="fas fa-list-ol rightSide-button-icon"></i>
+          <span className='rightSide-button-label'> Drafted <br/> Players </span>
+        </button>
       </div>
     </div>
   )
